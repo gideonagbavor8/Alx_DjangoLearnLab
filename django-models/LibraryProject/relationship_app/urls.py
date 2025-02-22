f7548ab3a03d78b8000e20a38abb4a307c59d32b
gideonagbavor8 <gagbavor@byupathway.edu>
2025-02-22 08:59:56 +0000

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, home
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import CustomLoginView, CustomLogoutView, register, list_books, LibraryDetailView, home
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
+# from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            login(request, user)
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
@@ -19,12 +22,27 @@ def list_books(request):
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 def home(request):
-    return redirect('list_books')
+    return render(request, 'list_books')
 
-from django.views.generic.detail import DetailView
-from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# Login View
+from django.contrib.auth import views as auth_views
+
+class CustomLoginView(auth_views.LoginView):
+    template_name = 'login.html'
+
+
+# Logout View
+class CustomLogoutView(auth_views.LogoutView):
+    template_name = 'logout.html'
+
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
