a32eec2bd76aa289499b2e43e0b0d1294eb8cf1e
gideonagbavor8 <gagbavor@byupathway.edu>
2025-02-25 14:04:48 +0000

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -5,25 +5,25 @@ from .models import CustomUser
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
-    # model = CustomUser
-    # list_display = ['email', 'date_of_birth', 'is_staff', 'is_active']
-    # list_filter = ['email', 'is_staff', 'is_active']
-    # fieldsets = (
-    #     (None, {'fields': ('email', 'password')}),
-    #     ('Personal info', {'fields': ('date_of_birth', 'profile_photo')}),
-    #     ('Permissions', {'fields': ('is_staff', 'is_active')}),
-    #     ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    # )
+    model = CustomUser
+    list_display = ['email', 'date_of_birth', 'is_staff', 'is_active']
+    list_filter = ['email', 'is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
-    # add_fieldsets = (
-    #     (None, {
-    #         'classes': ('wide',),
-    #         'fields': ('email', 'date_of_birth', 'password1', 'password2', 'is_staff', 'is_active')}
-    #      ),
-    # )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
 
-    # search_fields = ('email',)
-    # ordering = ('email',)
+    search_fields = ('email',)
+    ordering = ('email',)
 
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
