3e3f03c95609a3e154a38642a6c1541c1fac93b6
gideonagbavor8 <gagbavor@byupathway.edu>
2025-02-25 13:56:04 +0000

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -68,35 +68,40 @@ def save_user_profile(sender, instance, **kwargs):
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, email, password, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, username, password, **extra_fields)
     
 class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField(unique=True)
+    # username = None
+    # email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos', null=True, blank=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['date_of_birth']
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['date_of_birth']
 
     objects = CustomUserManager()
 
-    def __str__(self):
-        return self.email
+    # def __str__(self):
+        # return self.email
     
 from django.conf import settings
 
 class SomeModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+
+
+
