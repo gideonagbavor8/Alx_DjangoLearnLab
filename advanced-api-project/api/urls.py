457cc9cdb9a67ae0068fe8852a4b7f00b201bce7
gideonagbavor8 <gagbavor@byupathway.edu>
2025-03-13 21:23:27 +0000

    Writing Unit Tests for Django REST Framework APIs

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,3 +1,14 @@
+# from django.urls import path
+# from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+
+# urlpatterns = [
+#     path('books/', BookListView.as_view(), name='book-list'),
+#     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+#     path('books/create/', BookCreateView.as_view(), name='book-create'),
+#     path('books/update/', BookUpdateView.as_view(), name='book-update'),
+#     path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
+# ]
+
 from django.urls import path
 from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
@@ -5,6 +16,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
