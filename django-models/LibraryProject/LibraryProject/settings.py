6ac146b87e0d1ae4a4285a090f6f1cb6f45768c7
gideonagbavor8 <gagbavor@byupathway.edu>
2025-03-19 20:08:54 +0000

    Initial Setup and Project Configuration for a Django Blog

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',  # Placeholder
+        'PORT': '',  # Placeholder
+    }
+}
 
 import os
 
