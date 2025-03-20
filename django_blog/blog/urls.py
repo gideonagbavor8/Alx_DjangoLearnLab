6ff831db42fc7df5c878840a098e003f626e0f68
gideonagbavor8 <gagbavor@byupathway.edu>
2025-03-20 00:30:07 +0000

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,25 +9,25 @@
 # ]
 
 
-# from django.contrib.auth import views as auth_views
-# from django.urls import path
-# from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import path
+from . import views
 
-# urlpatterns = [
-#     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-#     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-#     path('register/', views.register, name='register'),
-#     path('profile/', views.profile, name='profile'),
-# ]
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
 
 from django.urls import path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-new'),
-    path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-new'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
