4b9ccef45d84e0eb75260e727f6009eeb5702f61
gideonagbavor8 <gagbavor@byupathway.edu>
2025-03-26 23:26:12 +0000

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,18 +2,27 @@ from rest_framework import serializers
 from accounts.models import CustomUser
 from rest_framework.authtoken.models import Token
 
+from django.contrib.auth import get_user_model
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password']
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
         extra_kwargs = {
             'password': {'write_only': True},
+            'followers': {'read_only': True},  # Prevents "followers" from being required
         }
 
-    def validate_username(self, value):
-        if CustomUser.objects.filter(username=value).exists():
-            raise serializers.ValidationError("This username is already taken. Please choose another.")
-        return value
+    def create(self, validated_data):
+        # Use get_user_model() to create a user
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data['email'],
+        )
+        Token.objects.create(user=user)  # Generate a token for the user upon registration
+        return user
+
 
 
 
