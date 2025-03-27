35a8e821ae71741aac03e8d5f0a22e0d3b25088c
gideonagbavor8 <gagbavor@byupathway.edu>
2025-03-27 00:24:51 +0000

    Implementing Posts and Comments Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,30 +1,48 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing posts with CRUD operations.
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    pagination_class = PageNumberPagination  # Enable pagination
+    filter_backends = [SearchFilter]         # Enable filtering
+    search_fields = ['title', 'content']     # Allow filtering posts by title or content
 
     def perform_create(self, serializer):
+        """
+        Automatically set the author of the post to the logged-in user.
+        """
         serializer.save(author=self.request.user)
 
-class CommentViewSet(ModelViewSet):
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing comments with CRUD operations.
+    """
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
+        """
+        Automatically set the author of the comment to the logged-in user.
+        """
         serializer.save(author=self.request.user)
 
 
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.filters import SearchFilter
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
