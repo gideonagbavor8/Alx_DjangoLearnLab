fd3a1e5ca28f86a969a8d23adbfb34fc6db1b30f
gideonagbavor8 <gagbavor@byupathway.edu>
2025-03-27 00:15:33 +0000

    Implementing Posts and Comments Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('comments', CommentViewSet)
+router.register('posts', PostViewSet, basename='posts')
+router.register('comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('', include(router.urls)),
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,11 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -14,7 +15,7 @@ class PostViewSet(ModelViewSet):
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
